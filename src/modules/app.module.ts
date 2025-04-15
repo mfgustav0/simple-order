e2236a25dad0826fe82b1af57c1ff14fead3602b
Gustavo Favaro <83651026+mfgustav0@users.noreply.github.com>
2025-04-15 13:38:49 -0300

    Add files via upload

---
+++ src/modules/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from './modules/users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_TOKEN'),
+        signOptions: { expiresIn: '300s' },
+      }),
+      inject: [ConfigService],
+    }),
+    ProductsModule,
+    OrdersModule,
+    UsersModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
