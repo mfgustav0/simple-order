fa82e3e62601a1d8f5dae3f300575eb1899e7b30
Gustavo Favaro <83651026+mfgustav0@users.noreply.github.com>
2025-04-16 09:02:01 -0300

    Add files via upload

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,14 +1,13 @@
 export class CreateUserDto {
-    readonly name: string;
-  
-    readonly email: string;
-  
-    readonly password: string;
-  
-    constructor(name: string, email: string, password: string) {
-      this.name = name;
-      this.email = email;
-      this.password = password;
-    }
+  readonly name: string;
+
+  readonly email: string;
+
+  readonly password: string;
+
+  constructor(name: string, email: string, password: string) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
   }
-  
+}
