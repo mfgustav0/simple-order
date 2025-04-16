c0a5623ed74d38fea7316505be7d0a33b72b0462
Gustavo Favaro <83651026+mfgustav0@users.noreply.github.com>
2025-04-16 11:00:19 -0300

    Create remove-item-order.use-case.ts

---
+++ src/modules/orders/use-cases/items/remove-item-order.use-case.ts
@@ -0,0 +1,43 @@
+import { OrderRepository } from '../../repositories/order.repository';
+import {
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
+import { OrderStatus } from '../../enums/order.status';
+import { ProductsService } from 'src/modules/products/products.service';
+
+@Injectable()
+export class RemoveItemOrder {
+  constructor(
+    readonly orderRepository: OrderRepository,
+    readonly productService: ProductsService,
+  ) {}
+
+  async execute(input: Input): Promise<void> {
+    const order = await this.orderRepository.findById(input.orderId);
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+
+    if (order.status !== OrderStatus.Pendent) {
+      throw new NotAcceptableException('Order not allowed to append item');
+    }
+
+    const productIndex = order.items.findIndex(
+      (item) => item.id === input.itemId,
+    );
+    if (productIndex < 0) {
+      throw new NotFoundException('Item not found');
+    }
+
+    order.items.splice(productIndex, 1);
+
+    await this.orderRepository.updateItems(order);
+  }
+}
+
+type Input = {
+  orderId: string;
+  itemId: string;
+};
